e4cb42036598ba711f10217a4b9275bc88c83732
Nowocinski <vito24222@gmail.com>
2022-05-31 21:12:38 +0200

    Create Planet.java

---
+++ app/src/main/java/com/example/android_images_glide/Planet.java
@@ -0,0 +1,58 @@
+package com.example.android_images_glide;
+
+public class Planet {
+    private String position;
+    private String name;
+    private String imageURl;
+    private String velocity;
+    private String distance;
+    private String description;
+
+    public String getPosition() {
+        return position;
+    }
+
+    public void setPosition(String position) {
+        this.position = position;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getImageURl() {
+        return imageURl;
+    }
+
+    public void setImageURl(String imageURl) {
+        this.imageURl = imageURl;
+    }
+
+    public String getVelocity() {
+        return velocity;
+    }
+
+    public void setVelocity(String velocity) {
+        this.velocity = velocity;
+    }
+
+    public String getDistance() {
+        return distance;
+    }
+
+    public void setDistance(String distance) {
+        this.distance = distance;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
